8140643e5b0f629aa394df73fc2f2794e641a048
Fanila Ali Agha <fanilaali99@gmail.com>
2022-03-13 10:58:56 -0700

    Bar_code_scanner_in_Flutter
    Barr code scanner in flutter

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+// import 'package:code_scanner/home.dart';
+import 'package:flutter/material.dart';
+import 'package:hello/home.dart';
+
+void main() => runApp(const MaterialApp(home: MyHome()));
+
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Demo Home Page')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => const QRViewExample(),
+            ));
+          },
+          child: const Text('qrView'),
+        ),
+      ),
+    );
+  }
+}
